196c66d762af22fbcfcdd7c00aadfec9baca0c73
elroy <Elroywang@UNIVERSEACTION.com>
2024-09-05 10:22:34 +0800

    更新壁纸

--- js/main.js
+++ js/main.js
@@ -50,37 +50,16 @@ $(document).ready(function () {
 	 * 改用 JsonBird：https://bird.ioliu.cn/
 	 * 
 	 */
-	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
-	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
-	var index = sessionStorage.getItem("index");
+	var url = 'https://bing.biturl.top/?resolution=UHD&format=json&index=0&mkt=zh-CN';
 	var $panel = $('#panel');
-	if (imgUrls == null) {
-		imgUrls = new Array();
-		index = 0;
 		$.get(url, function (result) {
-			images = result.images;
-			for (let i = 0; i < images.length; i++) {
-				const item = images[i];
-				imgUrls.push(item.url);
-			}
-			var imgUrl = imgUrls[index];
-			var url = "https://www.bing.com" + imgUrl;
+			imgUrls=result.url;
+			var url = result.url;
 			$panel.css("background", "url('" + url + "') center center no-repeat #666");
 			$panel.css("background-size", "cover");
-			sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
-			sessionStorage.setItem("index", index);
+
 		});
-	} else {
-		if (index == 7)
-			index = 0;
-		else
-			index++;
-		var imgUrl = imgUrls[index];
-		var url = "https://www.bing.com" + imgUrl;
-		$panel.css("background", "url('" + url + "') center center no-repeat #666");
-		$panel.css("background-size", "cover");
-		sessionStorage.setItem("index", index);
-	}
+
 
 	$(".iUp").each(function (i, e) {
 		iUp.up(e);
